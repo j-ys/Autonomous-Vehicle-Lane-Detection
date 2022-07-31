6b89546dab200bf591683a107b79e12d54034abd
j-ys <0jysjis0@kyonggi.ac.kr>
2022-07-31 16:17:27 +0900

    draft_version

---
+++ build/xycar_msgs/CMakeFiles/xycar_msgs_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xycar_msgs_generate_messages_cpp"
+  "/home/jys/catkin_ws/devel/include/xycar_msgs/xycar_motor.h"
+  "/home/jys/catkin_ws/devel/include/xycar_msgs/xycar_ultrasounds.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/xycar_msgs_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ devel/share/xycar_msgs/cmake/xycar_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(xycar_msgs_MESSAGE_FILES "/home/jys/catkin_ws/src/xycar_msgs/msg/xycar_motor.msg;/home/jys/catkin_ws/src/xycar_msgs/msg/xycar_ultrasounds.msg")
+set(xycar_msgs_SERVICE_FILES "")
